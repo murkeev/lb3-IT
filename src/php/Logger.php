99916e8ddc740d093e212fd8bb52e6cbac09114a
murkeev <disamurkeev@gmail.com>
2025-05-26 17:37:42 +0300

    fix

--- src/php/Logger.php
+++ src/php/Logger.php
@@ -7,13 +7,13 @@ class Logger {
         $this->pdo = $database->getConnection();
     }
 
-    public function logRequest($path, $param1 = null) {
+    public function logRequest($path, $param = null) {
         $sql = "INSERT INTO server_logs (request_path, param, request_time)
                 VALUES (:path, :param, NOW())";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([
             'path' => $path,
-            'param' => $param1
+            'param' => $param
         ]);
     }
 }
--- src/php/queries/logger.php
+++ src/php/queries/logger.php
@@ -7,16 +7,14 @@ try {
 
     $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    $param1 = $_GET['param1'] ?? null;
-    $param2 = $_GET['param2'] ?? null;
+    $param = $_GET['param1'] ?? null;
 
     $requestTime = date('Y-m-d H:i:s');
 
-    $stmt = $pdo->prepare("INSERT INTO request_logs (request_path, param1, param2, request_time) VALUES (:path, :p1, :p2, :time)");
+    $stmt = $pdo->prepare("INSERT INTO request_logs (request_path, param, request_time) VALUES (:path, :p1, :time)");
     $stmt->execute([
         ':path' => $requestPath,
-        ':p1' => $param1,
-        ':p2' => $param2,
+        ':p1' => $param,
         ':time' => $requestTime
     ]);
 } catch (PDOException $e) {
